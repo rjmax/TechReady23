9a2213a31715251a9b91d0fdfd6916ddd055edee
Ryan Jones <ryan.d.jones@microsoft.com>
2016-07-22 17:55:04 -0700

    removing the hack

--- tests/validate.ps1
+++ tests/validate.ps1
@@ -2,8 +2,5 @@
     [string] [Parameter(Mandatory=$true)] $TemplateFile,
     [string] [Parameter(Mandatory=$true)] $rg
     )
-    
-#Get-Module -ListAvailable
-#Get-Module -Name AzureRM.Resources
 
 Test-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $TemplateFile git -Verbose
