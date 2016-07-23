054857caa5373052299586acd6b5931aefb73898
Ryan Jones <ryan.d.jones@microsoft.com>
2016-07-22 18:08:03 -0700

    fixing stupid mistake in validate

--- tests/validate.ps1
+++ tests/validate.ps1
@@ -1,1 +1,6 @@
-﻿
+﻿Param(
+    [string] [Parameter(Mandatory=$true)] $TemplateFile,
+    [string] [Parameter(Mandatory=$true)] $rg
+    )
+
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $TemplateFile -Verbose
