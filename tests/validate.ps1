9fa11636d7ca65a87b39e6f4f52d9b88864f41dc
Ryan Jones <ryan.d.jones@microsoft.com>
2016-07-22 17:38:16 -0700

    adding preflight validation scripts

---
+++ tests/validate.ps1
@@ -0,0 +1,7 @@
+﻿Param(
+    [string] [Parameter(Mandatory=$true)] $TemplateUri,
+    [string] [Parameter(Mandatory=$true)] $TemplateParameterUri,
+    [string] [Parameter(Mandatory=$true)] $rg
+    )
+    
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateUri $TemplateUri -TemplateParameterUri $TemplateParameterUri -Verbose
