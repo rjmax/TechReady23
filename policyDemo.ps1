bd3244e768a2236de75ecb380320eebcd0ae7390
Ryan Jones <ryan.d.jones@microsoft.com>
2016-07-22 17:35:21 -0700

    adding policy demo comments

--- policyDemo.ps1
+++ policyDemo.ps1
@@ -1,4 +1,5 @@
-﻿$policy = New-AzureRmPolicyDefinition -Name "USOnlyPolicy" -Description "Resource in US Only" -Policy ‘{
+﻿#create the policy definition
+$policy = New-AzureRmPolicyDefinition -Name "USOnlyPolicy" -Description "Resource in US Only" -Policy ‘{
   "if": {
     "not": {
       "field": "location",
@@ -21,7 +22,9 @@
 
 $scope = "/subscriptions/9167af2d-c13e-4d34-9a57-8f37dba6ff31”
 
+#create the policy assignment
 New-AzureRmPolicyAssignment -Name "USOnlyAssignment" -PolicyDefinition $policy -Scope $scope
 
+#this should fail based on the policy/definitions above, since the command tries to create a resource in a denied region
 New-AzureRmResource -Location westeurope -ResourceName asdf -ResourceGroupName blah -ResourceType Microsoft.Web/sites -Force
 
