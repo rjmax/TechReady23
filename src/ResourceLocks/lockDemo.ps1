c660070d94e69aa7658476cf868b13546058d136
Ryan Jones <ryan.d.jones@microsoft.com>
2016-07-25 09:34:54 -0700

    adding lock demos

---
+++ src/ResourceLocks/lockDemo.ps1
@@ -0,0 +1,7 @@
+﻿$TemplateUri = "https://raw.githubusercontent.com/rjmax/TechReady23/master/src/ResourceLocks/lockDemo.json"
+
+#Create resource group
+New-AzureRmResourceGroup -Name LockDemo -Location westus -Force
+
+#Deploy storage account and lock it
+New-AzureRmResourceGroupDeployment -ResourceGroupName LockDemo -TemplateUri $TemplateUri 
