5b0fded5421fa6b604a231683848b2ec30db0856
Ryan Jones <ryan.d.jones@microsoft.com>
2016-07-25 10:18:31 -0700

    updating Lock Demo script

--- src/ResourceLocks/lockDemo.ps1
+++ src/ResourceLocks/lockDemo.ps1
@@ -8,5 +8,28 @@ $deploymentName = Get-Random
 #Deploy storage account and lock it
 $deployment = New-AzureRmResourceGroupDeployment -ResourceGroupName LockDemo -TemplateUri $TemplateUri -Name $deploymentName
 
-#Get the storage account we just created
-$deployment.Outputs
+#echo Deployment info
+$deployment
+
+#Get the name of the storage account we just created
+$name = $deployment.Outputs.name.Value
+
+#Try to delete the storage account - this will fail
+Write-Host "Trying to delete the storage account"
+Remove-AzureRmStorageAccount -ResourceGroupName LockDemo -Name $name
+
+#See if the storage account still exists
+Write-Host "Does it exist?"
+if(Find-AzureRmResource -ResourceGroupNameContains LockDemo -ResourceType Microsoft.Storage/storageAccounts){write-host "Yes"} else{write-host "No"}
+
+#Remove the lock
+Write-Host "Removing the lock"
+Remove-AzureRmResourceLock -LockName myLock -ResourceGroupName LockDemo -ResourceType Microsoft.Storage/storageAccounts -ResourceName $name -Force
+
+#Try to delete the storage account - this will succeed
+Write-Host "Trying to delete the storage account"
+Remove-AzureRmStorageAccount -ResourceGroupName LockDemo -Name $name
+
+#See if the storage account still exists
+Write-Host "Does it exist?"
+if(Find-AzureRmResource -ResourceGroupNameContains LockDemo -ResourceType Microsoft.Storage/storageAccounts){write-host "Yes"} else{write-host "No"}
